eb1ab5f568dc629a52ec99bf75e21d022606ef1d
Nix3732 <eaz055@mail.ru>
2024-07-04 18:10:04 +0200

    Update xmain.py

---
+++ excel_part/xmain.py
@@ -0,0 +1,13 @@
+def MatchesInput(): #Обновление файла "Список матчей"
+    pass
+
+def Profile(): #Сбор информации из "База"
+    pass
+
+def MatchesOutput(): #Подсчет матчей из "Список матчей"
+    pass
+
+def RaitingInput(): #Обновление рейтинга КОФНТ в "База"
+    pass
+
+def Raiting
