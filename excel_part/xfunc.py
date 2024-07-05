2d8ef0b38ca429dc4fc42e5be3d1763cecf37b60
Nix3732 <eaz055@mail.ru>
2024-07-05 14:53:18 +0200

    .

--- excel_part/xfunc.py
+++ excel_part/xfunc.py
@@ -6,37 +6,74 @@ def RaitingFileUpdate(link:str):
     
     month = link.split("\\")[-1].split('.')[0]
     l = len(df_raiting_new_m['Фамилия Имя'])
+    b = len(df_base['Фамилия'])
     three = []
 
-    if l != len(df_base['Фамилия']):
+    if l != b:
         one = df_raiting_new_m['Фамилия Имя'].to_list()
         two = []
-        for i in range(len(df_base['Фамилия'])):
+        for i in range(b):
             two.append(f"{df_base['Фамилия'][i]} {df_base['Имя'][i]}")
         three = list(set(one)-set(two))
         
         for i in range(len(three)):
-            df_base.at[l, 'Фамилия'] = three[i].split(' ')[0]
-            df_base.at[l, 'Имя'] = three[i].split(' ')[1]
+            df_base.at[l+i, 'Фамилия'] = three[i].split(' ')[0]
+            df_base.at[l+i, 'Имя'] = three[i].split(' ')[1]
             for j in range(l):
                 if df_raiting_new_m['Фамилия Имя'].values[j] == three[i]:
-                    df_base.at[l, month] = df_raiting_new_m['Рейтинг'].values[j]
-    df_base.to_excel("D:\Git\TT\PingPong-Bot\data\Рейтинг.xlsx", index=False)
+                    df_base.at[l+i, month] = df_raiting_new_m['Рейтинг'].values[j]
 
     for i in range(l):
         surename = df_raiting_new_m['Фамилия Имя'].values[i].split(' ')[0]
+        surename = surename.replace(' ', '')
         name = df_raiting_new_m['Фамилия Имя'].values[i].split(' ')[1]
+        name = name.replace(' ', '')
         raiting = df_raiting_new_m['Рейтинг'].values[i]
         
-        for j in range(len(df_base['Фамилия'])-len(three)):
+        for j in range(b-len(three)):
             if f"{surename} {name}" == f"{df_base['Фамилия'][j]} {df_base['Имя'][j]}":
                 df_base.at[j, month] = raiting
                 continue
 
     df_base.to_excel("D:\Git\TT\PingPong-Bot\data\Рейтинг.xlsx", index=False)
 
-def RaitingKOFNTUpdate():
-    pass
+def RaitingKOFNTUpdate(link:str):
+    
+    df_base = pd.read_excel("D:\Git\TT\PingPong-Bot\data\База.xlsx")
+    df_raiting_new_m = pd.read_excel(link)
+    l = len(df_raiting_new_m['Фамилия Имя'])
+    b = len(df_base['Фамилия'])
+    three = []
+
+    if l != b:
+        one = df_raiting_new_m['Фамилия Имя'].to_list()
+        two = []
+        for i in range(b):
+            two.append(f"{df_base['Фамилия'][i]} {df_base['Имя'][i]}")
+        three = list(set(one)-set(two))
+
+        for i in range(len(three)):
+            df_base.at[l+i, 'Фамилия'] = three[i].split(' ')[0]
+            df_base.at[l+i, 'Имя'] = three[i].split(' ')[1]
+            for j in range(l):
+                if df_raiting_new_m['Фамилия Имя'].values[j] == three[i]:
+                    df_base.at[l+i, "Дата рождения"] = str(df_raiting_new_m['Дата рождения'].values[j]).split('T')[0]
+                    df_base.at[l+i, "Рейтинг КОФНТ"] = df_raiting_new_m['Рейтинг'].values[j]
+                    df_base.at[l+i, "Город"] = df_raiting_new_m["Город"].values[j]
+
+    for i in range(l):
+        surename = df_raiting_new_m['Фамилия Имя'].values[i].split(' ')[0]
+        surename = surename.replace(' ', '')
+        name = df_raiting_new_m['Фамилия Имя'].values[i].split(' ')[1]
+        name = name.replace(' ', '')
+        raiting = df_raiting_new_m['Рейтинг'].values[i]
+        
+        for j in range(b-len(three)):
+            if f"{surename} {name}" == f"{df_base['Фамилия'][j]} {df_base['Имя'][j]}":
+                df_base.at[j, "Рейтинг КОФНТ"] = raiting
+                continue
+
+    df_base.to_excel("D:\Git\TT\PingPong-Bot\data\База.xlsx", index=False)
 
 def RaitingFNTRUpdate():
     pass
@@ -49,3 +86,7 @@ def CategoryUpdate():
 
 def CityUpdate():
     pass
+
+
+
+RaitingFileUpdate("D:\Git\TT\PingPong-Bot\data\garbage\Рейтинг КОФНТ\Мужчины\Июнь 2024.xlsm")
